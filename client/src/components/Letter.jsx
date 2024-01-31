97526050df2a3e4009a28fbb52a612ff5ba50240
stuart <stuartmarshall@fixie3d.com>
2024-01-31 10:25:10 +0000

    formatting

--- client/src/components/Letter.jsx
+++ client/src/components/Letter.jsx
@@ -1,25 +1,26 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-function Letter({ text, currentHue, setCurrentHue }){
-    const [ letterColor, setLetterColor] = useState('#E3E3E3')
-    text = text.toUpperCase();
-    const handleClick = () => {
-        let hue = currentHue;
-        if(!currentHue){
-            hue = Math.floor(Math.random() * 357);
-        } else {
-            hue = (currentHue + 15) % 357;
-            if (hue === 0) hue = 1;
-        }
-        setCurrentHue(hue)
-        setLetterColor(`hsl(${hue}, 63%, 62%)`);
-    }
+function Letter({ text, currentHue, setCurrentHue }) {
+	const [letterColor, setLetterColor] = useState('#E3E3E3');
+	text = text.toUpperCase();
 
-    return (
-        <div className="letter" style={{background: letterColor}} onClick={ handleClick }>
-            { text }
-        </div>
-    )
+	const handleClick = () => {
+		let hue = currentHue;
+		if (!currentHue) {
+			hue = Math.floor(Math.random() * 357);
+		} else {
+			hue = (currentHue + 15) % 357;
+			if (hue === 0) hue = 1;
+		}
+		setCurrentHue(hue);
+		setLetterColor(`hsl(${hue}, 63%, 62%)`);
+	};
+
+	return (
+		<div className='letter' style={{ background: letterColor }} onClick={handleClick}>
+			{text}
+		</div>
+	);
 }
 
 export default Letter;

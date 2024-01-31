9c93d74094e416f5f74d246aeca0029a61b58749
stuart <stuartmarshall@fixie3d.com>
2024-01-31 10:24:43 +0000

    move colour change to child

--- client/src/components/Gameboard.jsx
+++ client/src/components/Gameboard.jsx
@@ -4,18 +4,6 @@ import { useState } from "react";
 function Gameboard({ board }) {
     const [currentHue, setCurrentHue] = useState(null);
 
-    const updateCurrentHue = () => {
-        let hue = 0;
-        if(!currentHue){
-            hue = Math.floor(Math.random() * 357);
-        } else {
-            hue = (currentHue + 15) % 357;
-            if (hue === 0) hue = 1;
-        }
-        setCurrentHue(hue)
-        return hue;
-    }
-
     const gridStyle = {
         gridTemplateColumns: `repeat(${board[0].length}, 1fr)`,
         gridTemplateRows: `repeat(${board[0].length}, 1fr)`,
@@ -24,7 +12,7 @@ function Gameboard({ board }) {
         <section className="board-container mt-10" style={ gridStyle } aria-label="gameboard">
             { board.map((row) => {
                 return row.map((letter) => {
-                    return <Letter text={ letter } handleHue={ updateCurrentHue }  />
+                    return <Letter text={ letter } currentHue={ currentHue } setCurrentHue={ setCurrentHue }  />
                 })
             })}
         </section>
--- client/src/components/Letter.jsx
+++ client/src/components/Letter.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 
-function Letter({ text, handleHue }){
+function Letter({ text, currentHue, setCurrentHue }){
     const [ letterColor, setLetterColor] = useState('#E3E3E3')
     text = text.toUpperCase();
     const handleClick = () => {
-        const hue = handleHue();
+        let hue = currentHue;
+        if(!currentHue){
+            hue = Math.floor(Math.random() * 357);
+        } else {
+            hue = (currentHue + 15) % 357;
+            if (hue === 0) hue = 1;
+        }
+        setCurrentHue(hue)
         setLetterColor(`hsl(${hue}, 63%, 62%)`);
     }
 

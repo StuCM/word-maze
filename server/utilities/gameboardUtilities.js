a822856d625edc544c18e6722eb977ffb69eeb9f
stuart <stuartmarshall@fixie3d.com>
2024-02-09 15:23:41 +0000

    hook up wordnik api and create board with scores

--- server/utilities/gameboardUtilities.js
+++ server/utilities/gameboardUtilities.js
@@ -1,22 +1,34 @@
-async function getRandomWord(letters) {
-    try {
-        // const response = await fetch(`http://localhost:3000/words`)
-        // const data = await response.json();
-    
-        // const words = data[letters]
-        // const randomWord = words[Math.floor(Math.random() * words.length)]
-        return 'hello' 
-    } catch (error) {
-        console.error(error)
-        return null;
-    }
+const letterScores = require("./letterScores");
+
+async function getRandomWord(wordLength) {
+	try {
+		const apiKey = 'fgyd5zmp0gea651pdyd3djbmhx5y63meadu6096uxbt7jzbvh';
+		const response = await fetch(
+			`http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=${wordLength}&maxLength=${wordLength}&limit=1&api_key=${apiKey}`
+		);
+		const data = await response.json();
+		console.log(data);
+		return data[0];
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+}
+
+function addScores(word){
+	const letters = word.split('');
+	return letters.map((letter) => {
+		const score = letterScores[letter.toLowerCase()];
+		return {text:letter, score}
+	})
 }
 
 async function generateBoard(size, wordLength) {
 	const word = await getRandomWord(wordLength);
 	if (!word) return null;
 	let board = createEmptyBoard(size);
-	board = fillBoard(word, board);
+	const letterArray = addScores(word.word)
+	board = fillBoard(letterArray, board);
 	return board;
 }
 
@@ -31,15 +43,15 @@ function createEmptyBoard(size) {
 	return board;
 }
 
-function createWordPath(letters, board) {
+function createWordPath(letterArray, board) {
 	const randomIndex = (maxNumber) => {
 		return Math.floor(Math.random() * maxNumber);
 	};
 	let nextDirection = 'vertical';
-	let row = randomIndex(letters.length);
-	let column = randomIndex(letters.length);
-	board[row][column] = letters[0];
-	letters.slice(1).forEach((letter) => {
+	let row = randomIndex(letterArray.length);
+	let column = randomIndex(letterArray.length);
+	board[row][column] = letterArray[0];
+	letterArray.slice(1).forEach((letter) => {
 		if (nextDirection === 'vertical') {
 			do {
 				row = randomIndex(board.length);
@@ -57,14 +69,13 @@ function createWordPath(letters, board) {
 	return board;
 }
 
-function fillBoard(word, board) {
-	const letters = word.split('');
-	board = createWordPath(letters, board);
+function fillBoard(letterArray, board) {
+	board = createWordPath(letterArray, board);
 	board.forEach((row) => {
 		row.forEach((_, index) => {
 			if (row[index] !== '') return;
-			const randomIndex = Math.floor(Math.random() * letters.length);
-			row[index] = letters[randomIndex];
+			const randomIndex = Math.floor(Math.random() * letterArray.length);
+			row[index] = letterArray[randomIndex];
 		});
 	});
 	return board;

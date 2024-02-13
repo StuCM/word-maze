8e98e947fa53120961dfd71c5c32ebf07e36f93a
stuart <stuartmarshall@fixie3d.com>
2024-02-13 16:26:54 +0000

    fix modal close button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -168,18 +168,18 @@ function App() {
 					) : (
 						<HowToContent />
 					)}
-					<button className='py-2 px-3.5 bg-seconday m-4 rounded-full shadow-lg sticky bottom-0' onClick={handleModalClose}>
+					<button className='flex items-center justify-center mx-auto px-3 bg-seconday m-4 rounded-full shadow-lg sticky bottom-0' onClick={handleModalClose}>
 						{gameState === GAME_STATES.GAMEOVER && showScore ? (
-							<p className='text-textPrim font-semibold'>New Word</p>
+							<p className='text-textPrim font-semibold min-w-20'>New Word</p>
 						) : gameState === GAME_STATES.WIN && showScore ? (
-							<p className='text-textPrim font-semibold'>Try again?</p>
+							<p className='text-textPrim font-semibold min-w-20'>Try again?</p>
 						) : (
-							<FontAwesomeIcon icon={faX} className='text-lg text-textPrim' />
+							<FontAwesomeIcon icon={faX} className='text-md text-textPrim' />
 						)}
 					</button>
 					{gameState === GAME_STATES.WIN && showScore && (
-						<button className='py-2 px-3.5 bg-seconday m-4 rounded-full shadow-lg' onClick={restartGame}>
-							<p className='text-textPrim font-semibold'>New Word</p>
+						<button className='px-3.5 bg-seconday m-4 rounded-full shadow-lg' onClick={restartGame}>
+							<p className='text-textPrim font-semibold min-w-20'>New Word</p>
 						</button>
 					)}
 				</Modal>

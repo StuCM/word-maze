ac4bf0b54955e8d76ec7959406ae557bc31ff9c7
stuart <stuartmarshall@fixie3d.com>
2024-02-29 11:33:32 +0000

    fix modal button change on gamestate

--- client/src/components/ModalButtons.jsx
+++ client/src/components/ModalButtons.jsx
@@ -5,15 +5,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
 
 function ModalButtons({ handleModalClose, restartGame, showMenu }) {
-	const { gameState, gameMode } = useContext(GlobalState);
-
-	useEffect(() => {
-		console.log('gameState', gameMode);
-	}, [gameMode]);
+	const { gameState } = useContext(GlobalState);
 
 	return (
 		<>
-			{gameMode === 'daily' ? (
+			{gameState.gameMode === 'daily' ? (
 				<>
 					<button
 						className='px-3.5 bg-seconday m-4 rounded-full shadow-lg'
@@ -21,13 +17,13 @@ function ModalButtons({ handleModalClose, restartGame, showMenu }) {
 							handleModalClose();
 						}}
 					>
-						{gameState === GAME_STATES.WIN && gameState !== GAME_STATES.GAMEOVER ? (
+						{gameState.gameState === GAME_STATES.WIN && gameState.gameState !== GAME_STATES.GAMEOVER ? (
 							<p className='text-textPrim font-semibold min-w-20'>Try again?</p>
 						) : (
 							<FontAwesomeIcon icon={faX} className='text-md text-textPrim' />
 						)}
 					</button>
-					{(gameState === GAME_STATES.WIN || gameState === GAME_STATES.GAMEOVER) && (
+					{(gameState.gameState === GAME_STATES.WIN || gameState.gameState === GAME_STATES.GAMEOVER) && (
 						<button className='px-3.5 bg-seconday m-4 rounded-full shadow-lg' onClick={showMenu}>
 							<p className='text-textPrim font-semibold min-w-20'>Menu</p>
 						</button>
@@ -39,15 +35,15 @@ function ModalButtons({ handleModalClose, restartGame, showMenu }) {
 						className='flex items-center justify-center mx-auto px-3 bg-seconday m-4 rounded-full shadow-lg sticky bottom-0'
 						onClick={() => handleModalClose()}
 					>
-						{gameState === GAME_STATES.GAMEOVER ? (
+						{gameState.gameState === GAME_STATES.GAMEOVER ? (
 							<p className='text-textPrim font-semibold min-w-20'>New Word</p>
-						) : gameState === GAME_STATES.WIN ? (
+						) : gameState.gameState === GAME_STATES.WIN ? (
 							<p className='text-textPrim font-semibold min-w-20'>Try again?</p>
 						) : (
 							<FontAwesomeIcon icon={faX} className='text-md text-textPrim' />
 						)}
 					</button>
-					{gameState === GAME_STATES.WIN && (
+					{gameState.gameState === GAME_STATES.WIN && (
 						<button className='px-3.5 bg-seconday m-4 rounded-full shadow-lg' onClick={restartGame}>
 							<p className='text-textPrim font-semibold min-w-20'>New Word</p>
 						</button>
--- client/src/components/ScoreUI.jsx
+++ client/src/components/ScoreUI.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react';
+import { useContext } from 'react';
+import { GlobalState } from '../App';
 
-function ScoreUI({ score, attempts, children }) {
+function ScoreUI({ score, children }) {
+	const { gameState } = useContext(GlobalState)
 
 	return (
 		<section className='flex flex-col justify-center w-full items-center mt-4'>
 			<div className='flex items-center bg-seconday justify-around w-9/12 rounded-lg shadow-lg sm:w-80'>
 				<div className='text-center m-2.5'>
 					<p className='text-xl font-semibold'>Attempts</p>
-					<p className='text-3xl mt-1 mb-1 font-bold' data-testid="attempts">{attempts}</p>
+					<p className='text-3xl mt-1 mb-1 font-bold' data-testid="attempts">{gameState.remainingAttempts}</p>
 				</div>
 				<div className='text-center mx-6 my-2.5'>
 					<p className='text-xl font-semibold'>Score</p>

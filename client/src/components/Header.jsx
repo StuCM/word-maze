3efc329154aab3d97105e874562083f54507a55d
stuart <stuartmarshall@fixie3d.com>
2024-02-29 15:03:45 +0000

    change how to to menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleQuestion, faChartSimple } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faChartSimple } from '@fortawesome/free-solid-svg-icons';
 import logo from '../assets/Logo.png'
 import { GlobalState } from '../App';
 import { useContext } from 'react';
@@ -9,7 +9,6 @@ function Header() {
 
 	const openModal = (content) => {
 		dispatch({ type: 'OPEN_MODAL', payload: content})
-
 	}
 
 	return (
@@ -19,8 +18,8 @@ function Header() {
 			</button>
 			<img src={logo} alt="Logo" className="mr-2 h-8"/>
 			<h1 className='text-3xl font-semibold sm:text-4xl'>Muddle</h1>
-			<button className='absolute right-0 text-lg sm:text-2xl mr-1' onClick={() => openModal('help')}>
-				<FontAwesomeIcon icon={faCircleQuestion} />
+			<button className='absolute right-0 text-lg sm:text-2xl mr-1' onClick={() => openModal('menu')}>
+				<FontAwesomeIcon icon={faBars} />
 			</button>
 		</header>
 	);
